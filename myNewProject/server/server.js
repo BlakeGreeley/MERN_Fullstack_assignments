d845e35a0a49a6ed7c15d4b104335cabf6a921e8
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-08-09 11:12:45 -0700

    starting front end

--- myNewProject/server/server.js
+++ myNewProject/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+require('./routes/person.routes')(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
