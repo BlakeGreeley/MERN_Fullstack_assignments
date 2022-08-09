0ec850a778b0f208b7d45410e3fce27a9a9773c6
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-08-09 11:22:06 -0700

    adding cors to project

--- myNewProject/server/server.js
+++ myNewProject/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+app.use(cors()) //newest update
 require('./routes/person.routes')(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
