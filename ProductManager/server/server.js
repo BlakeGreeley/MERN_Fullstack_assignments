730d29a84c7d248cf8a988bfaeb3a89f32560b6f
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-08-10 08:45:45 -0700

    adding to product manager app

--- ProductManager/server/server.js
+++ ProductManager/server/server.js
@@ -5,3 +5,8 @@ const port = 8000;
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require('./config/mongoose.config');
+require('./routes/product.routes')(app);
