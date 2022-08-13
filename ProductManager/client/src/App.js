5fdf4f17b57e3b6717e1be79994f7238335f7a41
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-08-13 12:21:31 -0700

    adding to part 3

--- ProductManager/client/src/App.js
+++ ProductManager/client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Main from "./view/Main";
 import OneProduct from "./components/OneProduct";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UpdateProduct } from "../../server/controllers/product.controller";
 
 function App() {
     return (
@@ -9,7 +10,8 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path="/" element={<Main/>} />
-                    <Route path="/product/:id" element={<OneProduct/>}/> 
+                    <Route path="/product/:id" element={<OneProduct/>}/>
+                    <Route path='/product/edit/:id' element={<UpdateProduct/>} />
                 </Routes>
             </div>
         </BrowserRouter>
