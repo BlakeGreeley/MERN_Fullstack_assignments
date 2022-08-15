4d2060216506ca9f86de2cc8f18d8a814888ae79
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-08-15 14:29:16 -0700

    update 8/15/22

--- myNewProject/client/src/App.js
+++ myNewProject/client/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import React, { useState } from 'react';
-import axios from 'axios';
+// import React, { useState } from 'react';
+// import axios from 'axios';
 // import PersonForm from './components/PersonForm';
 import {BrowserRouter, Routes, Router} from 'react-router-dom';
 import Main from '../views/Main';
+import Detail from './components/Detail';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route element={<Main/>} path="/home" default/>
+          <Route element={<Main/>} path="/people/" />
+          <Route element = {<Detail/>} path="/people/:id" />
         </Routes>
       </BrowserRouter>
     </div>
--- myNewProject/server/controllers/person.controller.js
+++ myNewProject/server/controllers/person.controller.js
@@ -25,3 +25,9 @@
                 response.json(err)
             });
     };
+
+    module.exports.getPerson = (request, response) => {
+        Person.findOne({_id:request.params.id})
+            .then(person => response.json(person))
+            .catch(err => response.json(err))
+    };
