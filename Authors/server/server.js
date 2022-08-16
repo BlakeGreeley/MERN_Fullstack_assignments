d9c73aa74627f3d76f7977dd165947b3bf06a47e
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-08-16 13:16:59 -0700

    updated 8/16/22

--- Authors/server/server.js
+++ Authors/server/server.js
@@ -4,4 +4,6 @@ const port = 8000;
 const cors = require('cors');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => (`Listening to port: ${port}`));
