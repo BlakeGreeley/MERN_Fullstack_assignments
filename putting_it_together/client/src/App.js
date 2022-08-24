83cdf8c01590ca5609c6163dfa72e57b6bfe30a5
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-08-23 18:58:31 -0700

    added 8/23/22

---
+++ putting_it_together/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PersonCard from "./components/PersonCard";
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard
+        lastName={"Greeley"}
+      />
+    </div>
+  );
+}
+
+export default App;
